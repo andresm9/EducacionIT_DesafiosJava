c24c5f533543cfc4610fa0a77b51802e40de0252
Nestor Andres Martinez <nemartinez7@poligran.edu.co>
2023-07-12 20:04:27 -0500

    quitar comentarios

--- desafio1/Jsonify/src/main/java/com/educacionit/Jsonify.java
+++ desafio1/Jsonify/src/main/java/com/educacionit/Jsonify.java
@@ -4,8 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Jsonify {
     public static void main(String[] args) {
 
